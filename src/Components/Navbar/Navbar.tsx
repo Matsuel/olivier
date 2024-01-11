f771fc2c3b098b514041ed31940e9cf76c23b62b
Matsuel <matsuel0308@gmail.com>
2024-01-11 17:48:04 +0100

    navbar

--- src/Components/Navbar/Navbar.tsx
+++ src/Components/Navbar/Navbar.tsx
@@ -1,18 +1,20 @@
 import React from 'react'
 import './Navbar.css'
+import Olivier from '../../assets/olivier.png'
 
 const Navbar = () => {
     return (
         <div className='navbar-wrapper'>
             <div className="pseudo-wrap">
+                <img className='home-logo' src={Olivier} alt="Olivier" onClick={()=>window.location.href="/"} />
                 <a href='/' className='pseudo'>H1to</a>
-                <h1 className="pseudo">Oliver Tébar</h1>
             </div>
             <div className="nav-links">
                 <a className='link' href="#about">About</a>
                 <a className='link' href="#projects">Projects</a>
+            </div>
+            <div className="icth-link">
                 <button onClick={() => window.open('https://itch.io/profile/h1to', '_blank')} className='link-button'>Itch</button>
-                {/* <a className='link' target='_blank' href="https://itch.io/profile/h1to">Itch</a> */}
             </div>
         </div>
     )
