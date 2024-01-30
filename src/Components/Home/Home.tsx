2dbf62250cd070fd81dcb231584a81deabd6e79d
Matsuel <matsuel0308@gmail.com>
2024-01-30 09:25:02 +0100

    remake home

--- src/Components/Home/Home.tsx
+++ src/Components/Home/Home.tsx
@@ -1,4 +1,4 @@
-import React,{useEffect} from 'react'
+import React, { useEffect } from 'react'
 import './Home.css'
 import { useTranslation } from 'react-i18next';
 import { DarkModeContext } from '../../DarkModeContext';
@@ -6,12 +6,12 @@ import Olivier from '../../assets/olivier.png'
 import { TypeAnimation } from 'react-type-animation';
 
 const Home = () => {
-    const {darkMode} = React.useContext(DarkModeContext);
-    const { t } = useTranslation();      
+    const { darkMode } = React.useContext(DarkModeContext);
+    const { t } = useTranslation();
 
     return (
         <section id='about' className='home-wrapper'>
-            <img src={Olivier} alt="Olivier" className='home-logo-img'/>
+            <img src={Olivier} alt="Olivier" className='home-logo-img' />
             <h1 className={`home-title ${darkMode ? "home-title-light" : "home-title-dark"}`}>
                 <TypeAnimation
                     cursor={true}
@@ -27,6 +27,17 @@ const Home = () => {
                     repeat={Infinity}
                 />
             </h1>
+            <pre className={`home-text`} >
+                former R&D engineer during about 20 years. I have worked in different industries and been involved in a few major international projects. 
+                Despite my interest in this position, I fell the need to integrate an artistic side to my work.
+                So I started a retraining in 2021 in order to become a game designer. Both technical and artistic aspects mixed in one role, the best position i could imagine ever.
+                So here we are. 
+                I am gonna try to show you some of my creations. Thank you to take a look to my works. Additionnal contents will be available soon.
+                Breaking news : Climb demo update is out.  :)
+                Feel free to let constructive comments.
+                Any advice is really welcome.
+                Hito
+            </pre>
         </section>
     )
 }
