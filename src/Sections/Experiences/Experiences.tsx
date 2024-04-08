aa7b15b66254eb738c7ce6d9e1d071e72cbf8941
Matsuel <matsuel0308@gmail.com>
2024-04-08 21:05:52 +0200

    escape apos

--- src/Sections/Experiences/Experiences.tsx
+++ src/Sections/Experiences/Experiences.tsx
@@ -10,12 +10,12 @@ const Experiences = ({ }: ExperiencesProps) => {
     return (
         <section className={styles.Experiences_container} id="experiences">
             <h1 className={styles.Experiences_title}>
-                C'est les expériences là
+                C&apos;est les expériences là
             </h1>
 
             <p>
-                Ici y'aura des trucs sur les expériences
-                genre le nom de l'entreprise, le poste, les missions, les technologies utilisées, les dates, etc.
+                Ici y&apos;aura des trucs sur les expériences
+                genre le nom de l&apos;entreprise, le poste, les missions, les technologies utilisées, les dates, etc.
             </p>
             
         </section>
