896f531d9ef300a999fa58627beb580b8a2a645b
Matsuel <matsuel0308@gmail.com>
2024-06-03 21:15:19 +0200

    add background beams

--- src/sections/Hero.tsx
+++ src/sections/Hero.tsx
@@ -1,10 +1,9 @@
+import { BackgroundBeams } from '@/components/ui/background-beams'
 import React from 'react'
 
 const Hero = () => {
     return (
-        <section id="home" className='w-full h-[85vh] mt-[8%] flex items-center'>
-            {/* <div className='bg-[#fbe2e3] dark:bg-transparent absolute top-[8rem] -z-10 h-[31.25rem] w-[31.25rem] rounded-full blur-[10rem] sm:w-[68.75rem]' />
-            <div className='bg-[#dbd7fb] dark:bg-transparent absolute top-[7rem] -z-10 h-[31.25rem] w-[50rem] rounded-full blur-[10rem] sm:w-[68.75rem] md:left-[-33rem] lg:left-[-28rem] xl:left-[-15rem] 2xl:left-[-5rem]' /> */}
+        <section id="home" className='w-full h-[85vh] mt-[8%] flex items-center relative'>
 
             <div className='flex flex-row w-full h-full'>
                 <div className='w-1/2 flex flex-col items-center justify-center gap-20'>
@@ -18,6 +17,8 @@ const Hero = () => {
                     Image
                 </div>
             </div>
+
+            <BackgroundBeams />
         </section>
     )
 }
