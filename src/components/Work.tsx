48098d40e94dc1182bd3a9fc1679ba2e99f83f2e
Matsuel <matsuel0308@gmail.com>
2024-06-04 21:20:19 +0200

    experiences section

--- src/components/Work.tsx
+++ src/components/Work.tsx
@@ -1,6 +1,7 @@
 import { WorkType } from '@/datas/works'
 import Image from 'next/image'
 import React from 'react'
+import { PlugZap } from 'lucide-react'
 
 const Work = (props: WorkType) => {
     return (
@@ -8,7 +9,9 @@ const Work = (props: WorkType) => {
             {props.src !== "" ?
                 <Image src={props.src} alt={props.title} width={40} height={40} className='rounded-xl' />
                 :
-                <div className='w-[40px] h-[40px] bg-white rounded-xl'></div>
+                <div className='w-[40px] h-[40px] bg-white rounded-xl flex justify-center items-center'>
+                    <PlugZap size={20} strokeWidth={2} />
+                </div>
             }
             <div>
                 <h3>{props.title} at {props.company}</h3>
